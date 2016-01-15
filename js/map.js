4e3780204f267eb1dcf92571ec2d1fe73ca89d1e
Arun Ganesh <arun.planemad@gmail.com>
2016-01-15 17:25:25 +0530

    Create new map for OSM places

--- js/map.js
+++ js/map.js
@@ -4,25 +4,43 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
 var map = new mapboxgl.Map({
   container: 'map', // container id
-  style: 'mapbox://styles/planemad/cijcefp3q00elbskq4cgvcivf', //stylesheet location
+  style: 'mapbox://styles/planemad/cijfluvvd00229tm4zavbuvno', //stylesheet location
   hash: true
 });
+
+// Stylez
+// Place changes mapbox://styles/planemad/cijfluvvd00229tm4zavbuvno
+// osmlint mapbox://styles/planemad/cijcefp3q00elbskq4cgvcivf
 
 // Add geocoder https://github.com/mapbox/mapbox-gl-geocoder/blob/master/API.md
 map.addControl(new mapboxgl.Geocoder({'position':'top-right'}));
 
 map.on('style.load', function(e) {
 
+  // Display feature properties on hover
+  map.on('mousemove', function (e) {
+       map.featuresAt(e.point, {layer:'data circle', radius: 5}, function (err, features) {
+           if (err) throw err;
+           document.getElementById('feature-properties').innerHTML = JSON.stringify(features[0].properties, null, 2);
+       });
+   });
+
+  // Open in JOSM on click
   map.on('click', function(e) {
+
+    var osmIdField = {'way':'id','node':'id'}
+    // var osmIdField = {'way':'_osm_way_id','node':'_osm_node_id'}
+
     map.featuresAt(e.point, {
-      layer: ['osmlint circle'],
+      layer: ['data circle'],
       radius: 4
     }, function(err, features) {
 
       // Code lifted from https://github.com/geohacker/geojson-josm-url/blob/master/index.js
       var baseURL = "http://localhost:8111/load_object?new_layer=true&objects=";
-      var prefix = Object.keys(features[0].properties).indexOf('_osm_way_id') == -1 ? 'n' : 'w';
-      var osmID = features[0].properties._osm_way_id || features[0].properties._osm_node_id;
+
+      var prefix = Object.keys(features[0].properties).indexOf(osmIdField.node) == -1 ? 'w' : 'n';
+      var osmID = features[0].properties[osmIdField.node] || features[0].properties[osmIdField.way];
 
       window.open(baseURL + prefix + osmID);
     });
